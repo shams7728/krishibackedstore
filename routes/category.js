df9efaab052289f24054493a73a18b50b6ec530c
shams7728 <140730412+shams7728@users.noreply.github.com>
2025-03-30 16:13:27 +0530

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -48,7 +48,7 @@ router.post('/', asyncHandler(async (req, res) => {
             const { name } = req.body;
             let imageUrl = 'no_url';
             if (req.file) {
-                imageUrl = `https://krishibackedcode.onrender.com/image/category/${req.file.filename}`;
+                imageUrl = `https://krishibackedstore.onrender.com/image/category/${req.file.filename}`;
             }
             console.log('url ', req.file)
 
@@ -96,7 +96,7 @@ router.put('/:id', asyncHandler(async (req, res) => {
             let image = req.body.image;
 
             if (req.file) {
-                image = `https://krishibackedcode.onrender.com/image/category/${req.file.filename}`;
+                image = `https://krishibackedstore.onrender.com/image/category/${req.file.filename}`;
             }
 
             if (!name || !image) {
